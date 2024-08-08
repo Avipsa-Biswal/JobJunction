6f221f8dad3f9ddb206ea65982a838b67b237d97
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2024-08-08 17:44:00 +0530

    User Schema development

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,87 +1,95 @@
-import React from 'react'
-import Navbar from '../shared/Navbar'
-import { Label } from '../ui/label'
-import { Input } from '../ui/input'
-import { RadioGroup } from '../ui/radio-group'
-import { Button } from '../ui/button'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    const Login = () => {
-        const [input, setInput] = useState({
-            email: "",
-            password: "",
-            role: "",
-        });
-        const changeEventHandler = (e) => {
-            setInput({ ...input, [e.target.name]: e.target.value });
-        }
-      
-        const submitHandler = async (e) => {
-            e.preventDefault();
-        }
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
-        <Navbar/>
-    <div className='flex items-center justify-center max-w-7xl mx-auto'>
-    <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
-    <h1 className='font-bold text-xl mb-5'>Login</h1>
-   
-    <div className='my-2'>
-                        <Label>Email</Label>
-                        <Input
-                              type="text"
-                              value={input.fullname}
-                              name="fullname"
-                              onChange={changeEventHandler}
-                              placeholder="user"
-                        />
-                    </div>
-                   
-                    <div className='my-2'>
-                        <Label>Password</Label>
-                        <Input
-                             type="email"
-                             value={input.email}
-                             name="email"
-                             onChange={changeEventHandler}
-                             placeholder="abcd@gmail.com"
-                        />
-                    </div>
-                    <div className='flex items-center justify-between'>
-                    <RadioGroup className="flex items-center gap-4 my-5">
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="default" id="r1" />
-        <Input
-                                    type="radio"
-                                    name="role"
-                                    value="student"
-                                    onChange={changeEventHandler}
-                                    className="cursor-pointer"
-                                />
-        <Label htmlFor="r1">Student</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="comfortable" id="r2" />
-        <Input
-                                    type="radio"
-                                    name="role"
-                                    value="recruiter"
-                                    onChange={changeEventHandler}
-                                    className="cursor-pointer"
-                                />
-        <Label htmlFor="r2">Recruiter</Label>
-      </div>
-    </RadioGroup>
-                        </div>
-                        <Button type="submit" className="w-full my-4">Login</Button>
-                        <span className='text-sm'>Don't have an account? <Link to="/signup" className='text-blue-600'>Signup</Link></span>
-    </form>
-  </div>
-  </div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Login</h1>
 
-  )
-}
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input
+              type="text"
+              value={input.fullname}
+              name="fullname"
+              onChange={changeEventHandler}
+              placeholder="user"
+            />
+          </div>
 
-export default Login
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="abcd@gmail.com"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="default" id="r1" />
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="comfortable" id="r2" />
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <Button type="submit" className="w-full my-4">
+            Login
+          </Button>
+          <span className="text-sm">
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-blue-600">
+              Signup
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
