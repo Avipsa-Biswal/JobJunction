0075d8156d1df6083021724277596898513e1ac6
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2024-08-08 17:20:57 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({});
 const app = express();
 
@@ -23,5 +24,6 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
+    connectDB()
   console.log(`Server running at port ${PORT}`);
 });
