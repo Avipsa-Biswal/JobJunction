6b4b79b246aebdd4e36abb7c2a6484dced52547c
Kumarsab11 <138751580+Kumarsab11@users.noreply.github.com>
2024-08-08 17:04:50 +0530

    backend started

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+
+app.get("/home", (req, res) => {
+  return res.status(200).json({
+    message: "This result is coming from backend",
+    success: true,
+  });
+});
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http//localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
