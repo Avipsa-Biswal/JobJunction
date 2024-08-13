7df69939e7f5fb5f9922a351ac25d7a24f58e18e
Prangya <prangyajyotimohanty@gmail.com>
2024-08-13 20:31:25 +0530

    profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -4,9 +4,12 @@ import { Avatar, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
 import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
+import { Label } from './ui/label'
+import AppliedJobTable from './AppliedJobTable'
+import UpdateProfileDialog from './UpdateProfileDialog'
 import useGetAppliedJobs from '@/hooks/useGetAppliedJobs'
 
-// const skills = ["Html", "Css", "Javascript", "Reactjs"]
+
 const isResume = true;
 
 const Profile = () => {
