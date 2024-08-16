0c834ff9e48c563e9e0cdf74c43c24077c458ae6
Sidra <117569288+Sidra6666@users.noreply.github.com>
2024-08-17 00:20:40 +0530

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react'
 import { Button } from './ui/button'
 import { Search } from 'lucide-react'
-import { useDispatch } from 'react-redux';
-import { setSearchedQuery } from '@/redux/jobSlice';
-import { useNavigate } from 'react-router-dom';
+
 
 const HeroSection = () => {
     const [query, setQuery] = useState("");
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+
 
     const searchJobHandler = () => {
         dispatch(setSearchedQuery(query));
