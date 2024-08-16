72f8312cf6841eb629cfc07e85e741aff9c1f9a2
Sidra <117569288+Sidra6666@users.noreply.github.com>
2024-08-17 00:26:59 +0530

    Update CategoryCarousel.jsx

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,39 +1,39 @@
-import React from "react";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./ui/carousel";
-import { Button } from "./ui/button";
+import React from 'react';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel';
+import { Button } from './ui/button';
+
 
 const category = [
-  "Frontend Developer",
-  "Backend Developer",
-  "Data Science",
-  "Graphic Designer",
-  "Full Stack Developer",
-];
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "FullStack Developer"
+]
 
 const CategoryCarousel = () => {
-  return (
-    <div>
-      <Carousel className="w-full max-w-xl mx-auto my-20">
-        <CarouselContent>
-          {category.map((cat, index) => (
-            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
-              <Button variant="outline" className="rounded-full">
-                {cat}
-              </Button>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
-  );
-};
+    const searchJobHandler = (query) => {
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+    }
 
-export default CategoryCarousel;
+    return (
+        <div>
+            <Carousel className="w-full max-w-xl mx-auto my-20">
+                <CarouselContent>
+                    {
+                        category.map((cat, index) => (
+                            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                                <Button onClick={()=>searchJobHandler(cat)} variant="outline" className="rounded-full">{cat}</Button>
+                            </CarouselItem>
+                        ))
+                    }
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
+    )
+}
+
+export default CategoryCarousel
