5efc8a5f69d09f903953b8ac1ed2d8e2eb83f963
Prangya <prangyajyotimohanty@gmail.com>
2024-08-16 23:14:12 +0530

    profile page design

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
-import { Badge } from './ui/badge'
-import { useSelector } from 'react-redux'
+ import React from 'react'
+ import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+ import { Badge } from './ui/badge'
+ import { useSelector } from 'react-redux'
 
-const AppliedJobTable = () => {
-    const {allAppliedJobs} = useSelector(store=>store.job);
+ const AppliedJobTable = () => {
     return (
         <div>
             <Table>
@@ -19,12 +18,12 @@ const AppliedJobTable = () => {
                 </TableHeader>
                 <TableBody>
                     {
-                        allAppliedJobs.length <= 0 ? <span>You haven't applied any job yet.</span> : allAppliedJobs.map((appliedJob) => (
-                            <TableRow key={appliedJob._id}>
-                                <TableCell>{appliedJob?.createdAt?.split("T")[0]}</TableCell>
-                                <TableCell>{appliedJob.job?.title}</TableCell>
-                                <TableCell>{appliedJob.job?.company?.name}</TableCell>
-                                <TableCell className="text-right"><Badge className={`${appliedJob?.status === "rejected" ? 'bg-red-400' : appliedJob.status === 'pending' ? 'bg-gray-400' : 'bg-green-400'}`}>{appliedJob.status.toUpperCase()}</Badge></TableCell>
+                        [1,2,3,4].map((item, index) => (
+                            <TableRow key={index}>
+                                <TableCell>17-12-03</TableCell>
+                                <TableCell>Frontend developer</TableCell>
+                                <TableCell>Google</TableCell>
+                                <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
                             </TableRow>
                         ))
                     }
@@ -34,4 +33,4 @@ const AppliedJobTable = () => {
     )
 }
 
-export default AppliedJobTable
+ export default AppliedJobTable
