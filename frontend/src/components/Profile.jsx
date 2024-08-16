f9e55f00f7f2902445ac0c5d60d300ed4d3f8792
Sidra <117569288+Sidra6666@users.noreply.github.com>
2024-08-17 00:04:25 +0530

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './shared/Navbar'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
@@ -6,12 +6,18 @@ import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
 import AppliedJobTable from './AppliedJobTable'
+import UpdateProfileDialog from './UpdateProfileDialog'
+import { useSelector } from 'react-redux'
+import useGetAppliedJobs from '@/hooks/useGetAppliedJobs'
 
+// const skills = ["Html", "Css", "Javascript", "Reactjs"]
+const isResume = true;
 
-const skills = ["Html", "Css", "Javascript", "Reactjs"]
 const Profile = () => {
+    useGetAppliedJobs();
+    const [open, setOpen] = useState(false);
+    const {user} = useSelector(store=>store.auth);
 
-    const isResume = true;
     return (
         <div>
             <Navbar />
@@ -22,41 +28,43 @@ const Profile = () => {
                             <AvatarImage src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg" alt="profile" />
                         </Avatar>
                         <div>
-                            <h1 className='font-medium text-xl'>Full Name</h1>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam praesentium odio atque at culpa ex nisi? Maxime quaerat sit nesciunt corporis, reprehenderit est quisquam blanditiis, quibusdam numquam, quam iste veritatis!</p>
+                            <h1 className='font-medium text-xl'>{user?.fullname}</h1>
+                            <p>{user?.profile?.bio}</p>
                         </div>
                     </div>
-                    <Button className="text-right" variant="outline"><Pen /></Button>
+                    <Button onClick={() => setOpen(true)} className="text-right" variant="outline"><Pen /></Button>
                 </div>
                 <div className='my-5'>
                     <div className='flex items-center gap-3 my-2'>
                         <Mail />
-                        <span>prangya@gmail.com</span>
+                        <span>{user?.email}</span>
                     </div>
                     <div className='flex items-center gap-3 my-2'>
                         <Contact />
-                        <span>1235467890</span>
+                        <span>{user?.phoneNumber}</span>
                     </div>
                 </div>
                 <div className='my-5'>
                     <h1>Skills</h1>
-                    <div className='flex items-center gap-1'>    
+                    <div className='flex items-center gap-1'>
                         {
-                            skills.length !== 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>): <span>NA</span>
+                            user?.profile?.skills.length !== 0 ? user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
                         }
                     </div>
                 </div>
                 <div className='grid w-full max-w-sm items-center gap-1.5'>
                     <Label className="text-md font-bold">Resume</Label>
                     {
-                        isResume ? <a target='blank' href='https://youtube.com/@patelmernstack' className='text-blue-500 w-full hover:underline cursor-pointer'>Mern Stack</a> : <span>NA</span>
+                        isResume ? <a target='blank' href={user?.profile?.resume} className='text-blue-500 w-full hover:underline cursor-pointer'>{user?.profile?.resumeOriginalName}</a> : <span>NA</span>
                     }
                 </div>
             </div>
             <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
                 <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
+                {/* Applied Job Table   */}
                 <AppliedJobTable />
             </div>
+            <UpdateProfileDialog open={open} setOpen={setOpen}/>
         </div>
     )
 }
